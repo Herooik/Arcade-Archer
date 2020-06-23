0608ee42308459449448e1c9a122818181aa2c7c
Paweł <pawelkarpiel13@gmail.com>
2020-06-23 17:47:03 +0200

    enemy movement added, spawn manager fix

--- Arcade Archer/Assets/Scripts/InvadersSpawnManager.cs
+++ Arcade Archer/Assets/Scripts/InvadersSpawnManager.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 
 public class InvadersSpawnManager : MonoBehaviour
 {
+    public List<GameObject> spawnedEnemies;
+    
     [SerializeField] private float startPosX = -7f;
     [SerializeField] private float startPosY = 5;
     [SerializeField] private int enemiesToSpawnInRow = 10;
-    [SerializeField] private List<GameObject> enemies;
-    
+    [SerializeField] private List<GameObject> enemiesToUse;
+
     private int _enemiesToSpawnInColumn = 5;
     
     void Start()
     {
-        _enemiesToSpawnInColumn = enemies.Count;
+        _enemiesToSpawnInColumn = enemiesToUse.Count;
         for (int i = 0; i < _enemiesToSpawnInColumn; i++)
         {
             for (float j = 0; j < enemiesToSpawnInRow; j++)
             {
-                var invader = Instantiate(enemies[i]);
-                invader.name = "Invader " + j * i;
+                var invader = Instantiate(enemiesToUse[i]);
+                invader.name = "Invader " + j;
                 invader.transform.position = new Vector3(j + startPosX, startPosY - i);
+                
+                spawnedEnemies.Add(invader);
             }
         }
     }
