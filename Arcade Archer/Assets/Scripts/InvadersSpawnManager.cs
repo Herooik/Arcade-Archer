89e96b9e46f4ba7a6b69287aed9eeff54d9708d2
Paweł <pawelkarpiel13@gmail.com>
2020-06-23 15:37:29 +0200

    spawn manager

---
+++ Arcade Archer/Assets/Scripts/InvadersSpawnManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvadersSpawnManager : MonoBehaviour
+{
+    [SerializeField] private float startPosX = -7f;
+    [SerializeField] private float startPosY = 5;
+    [SerializeField] private int enemiesToSpawnInRow = 10;
+    [SerializeField] private List<GameObject> enemies;
+    
+    private int _enemiesToSpawnInColumn = 5;
+    
+    void Start()
+    {
+        _enemiesToSpawnInColumn = enemies.Count;
+        for (int i = 0; i < _enemiesToSpawnInColumn; i++)
+        {
+            for (float j = 0; j < enemiesToSpawnInRow; j++)
+            {
+                var invader = Instantiate(enemies[i]);
+                invader.name = "Invader " + j * i;
+                invader.transform.position = new Vector3(j + startPosX, startPosY - i);
+            }
+        }
+    }
+}
