4e7faf4ec45b7dd50d3bad247e2cd62042e52efd
Zych07 <39556481+Zych07@users.noreply.github.com>
2020-06-29 16:51:53 +0200

    29.06

---
+++ Arcade-Archer/Assets/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Animator anim;
+
+    [SerializeField]
+    float _deadZone = 0.1f;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        float velX = Input.GetAxis("Vertical");
+        float velY = Input.GetAxis("Horizontal");
+
+        if(Mathf.Abs(velX) < _deadZone) velX=0f;
+        if(Mathf.Abs(velY) < _deadZone) velY=0f;
+
+        Move(velX, velY);
+    }
+
+    private void Move(float x, float y) 
+    {
+        anim.SetFloat("velX", x);
+        anim.SetFloat("velY", y);
+    }
+}
