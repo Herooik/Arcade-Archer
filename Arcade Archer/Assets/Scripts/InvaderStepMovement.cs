27b1d0ecd46af32de4951cf731d673c30a7d315e
Paweł <pawelkarpiel13@gmail.com>
2020-07-06 18:20:33 +0200

    spawn, movement fix

---
+++ Arcade Archer/Assets/Scripts/InvaderStepMovement.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvaderStepMovement : MonoBehaviour
+{
+    [SerializeField] private InvadersSpawnManager invadersList;
+    [SerializeField][Range(0, 2)] private float pauseBetweenStep = 0.5f;
+    [SerializeField][Range(0, 2)] private float stepDistance = 0.2f;
+    [SerializeField][Range(0, 2)] private float changeColumnDistance = 0.5f;
+    [SerializeField] private float rightBorderX = 7f;
+    [SerializeField] private float leftBorderX = -7f;
+    
+    private bool moveInfinity = true;
+    private bool _changeColumn;
+
+    private IEnumerator Start()
+    {
+        do
+        {
+            yield return StartCoroutine(Moving());
+        } while (moveInfinity);
+    } 
+
+    private IEnumerator Moving()
+    {
+        yield return new WaitForSeconds(pauseBetweenStep);
+
+        MoveEnemy();
+
+        yield return new WaitForSeconds(pauseBetweenStep);
+
+        ChangeDirection();
+
+        if (_changeColumn)
+        {
+            ChangeColumn();
+            _changeColumn = false;
+        }
+    }
+    
+    private void MoveEnemy()
+    {
+        foreach (var enemy in invadersList.spawnedEnemies)
+        {
+            enemy.transform.position += Vector3.right * stepDistance;
+        }
+    }
+    
+    private void ChangeDirection()
+    {
+        foreach (var enemy in invadersList.spawnedEnemies)
+        {
+            if (enemy.transform.position.x >= rightBorderX)
+            {
+                stepDistance = -stepDistance;
+                _changeColumn = true;
+            }
+            else if (enemy.transform.position.x <= leftBorderX)
+            {
+                stepDistance = -stepDistance;
+                _changeColumn = true;
+            }
+        }
+    }
+    
+    private void ChangeColumn()
+    {
+        foreach (var enemy in invadersList.spawnedEnemies) 
+        {
+            var newPos = new Vector3(enemy.transform.position.x, enemy.transform.position.y - changeColumnDistance);
+            enemy.transform.position = newPos;
+        }
+    }
+}
--- Arcade Archer/Assets/Scripts/InvadersSpawnManager.cs
+++ Arcade Archer/Assets/Scripts/InvadersSpawnManager.cs
@@ -1,31 +1,46 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class InvadersSpawnManager : MonoBehaviour
 {
     public List<GameObject> spawnedEnemies;
-    
-    [SerializeField] private float startPosX = -7f;
-    [SerializeField] private float startPosY = 5;
+
+    [SerializeField] private GameObject startSpawnPosition;
     [SerializeField] private int enemiesToSpawnInRow = 10;
+    [SerializeField] private GameObject invaderContainer;
     [SerializeField] private List<GameObject> enemiesToUse;
 
     private int _enemiesToSpawnInColumn = 5;
     
     void Start()
     {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    IEnumerator SpawnEnemies()
+    {
         _enemiesToSpawnInColumn = enemiesToUse.Count;
+
         for (int i = 0; i < _enemiesToSpawnInColumn; i++)
         {
             for (float j = 0; j < enemiesToSpawnInRow; j++)
             {
                 var invader = Instantiate(enemiesToUse[i]);
-                invader.name = "Invader " + j;
-                invader.transform.position = new Vector3(j + startPosX, startPosY - i);
+                
+                var newInvaderPos = new Vector2(startSpawnPosition.transform.position.x + j, startSpawnPosition.transform.position.y - i);
+                invader.transform.position = newInvaderPos;
+                invader.transform.SetParent(invaderContainer.transform);
                 
                 spawnedEnemies.Add(invader);
+                invader.name = "Invader " + spawnedEnemies.Count;
+                
+                yield return new WaitForSeconds(0.1f);
             }
         }
+
+        GetComponent<InvaderStepMovement>().enabled = true;
     }
 }
